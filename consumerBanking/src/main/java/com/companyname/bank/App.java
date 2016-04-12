ca1f3a7625705c1ac2e4a256c301b6ac3cfb7cf5
KrishEngr <krishnendumohapatra2@gmail.com>
2016-04-12 19:35:24 +0530

    final updated with JUnit test case

--- consumerBanking/src/main/java/com/companyname/bank/App.java
+++ consumerBanking/src/main/java/com/companyname/bank/App.java
@@ -1,21 +1,58 @@
 package com.companyname.bank;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Maven TEST RUN successful" );
-        System.out.println("check");
-        System.out.println("check done");
-        System.out.println("final done and check");
-        System.out.println("km check");
-        System.out.println("km check 1");
-        System.out.println("km check 2");
-        
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class App {
+	static int N, K;
+	static long arr[];
+	public static void main(String args[]) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			N = Integer.parseInt(br.readLine().trim());
+
+			K = Integer.parseInt(br.readLine().trim()) - 1;
+			arr = new long[N];
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(br.readLine().trim());
+			}
+			max_min(N, K, arr);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		/*
+		 * N=7 K=3 
+		 * arr=10 100 300 200 1000 20 30
+		 */
+
+	}
+
+	public static void max_min(int N, int K, long arr[]) {
+		System.out.println("enter max_min");
+
+		String list = "";
+		Arrays.sort(arr);
+		for (long z : arr) {
+			list = list + " " + z;
+
+		}
+
+		System.out.println(list);
+		long unfairness = Long.MAX_VALUE;
+		for (int i = 0; i + K < N; i++) {
+			System.out.println(arr[i + K] + "-" + arr[i]);
+			long diff = arr[i + K] - arr[i];
+			if (unfairness > diff)
+				unfairness = diff;
+
+		}
+		System.out.println(unfairness);
+
+	}
+
 }
--- consumerBanking/src/test/java/com/companyname/bank/AppTest.java
+++ consumerBanking/src/test/java/com/companyname/bank/AppTest.java
@@ -1,39 +1,51 @@
 package com.companyname.bank;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+public class AppTest {
+	 
+	@Before
+    public void testcaseFirst()
+    {
+        App.N = 7;
+        App.K = 3;
+        long[] temp = {10l, 100l, 300l, 200l, 1000l, 20l, 30l};
+        App.arr = temp;
+    }
+    
+    @Test
+    public void testcaseSecond()
+    {
+    	
+    	App.max_min(App.N, App.K, App.arr);
+        
+    }
+
+
+}
+
+/*
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+  
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+ 
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+ 
     public void testApp()
     {
         assertTrue( true );
     }
 }
 
+*/
