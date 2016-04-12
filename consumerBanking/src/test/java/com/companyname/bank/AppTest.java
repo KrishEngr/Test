7611648b7702cde844bf7c6a34b70ad74a67fd2d
KrishEngr <krishnendumohapatra2@gmail.com>
2016-04-12 19:37:05 +0530

    Update AppTest.java

--- consumerBanking/src/test/java/com/companyname/bank/AppTest.java
+++ consumerBanking/src/test/java/com/companyname/bank/AppTest.java
@@ -24,28 +24,3 @@ public class AppTest {
 
 
 }
-
-/*
-public class AppTest 
-    extends TestCase
-{
-  
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
- 
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
- 
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
-
-*/
